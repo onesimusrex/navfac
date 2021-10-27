e3ac9eda99097052199385c514ff54353bfe1fd7
David <onesimusrex@gmail.com>
2021-10-27 17:08:46 -0400

    added html/data/data.js

---
+++ public/html/data/data.js
@@ -0,0 +1,90 @@
+var data = {
+
+    "essentialtask": {
+        "fms": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        },
+        "transportation": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        },
+        "um": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        }
+    },
+    "training": {
+        "fms": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        },
+        "transportation": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        },
+        "um": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        }
+    },
+    "credential": {
+        "fms": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        },
+        "transportation": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        },
+        "um": {
+            data: [
+                "item1",
+                "item2"
+            ]
+        }
+    }
+
+}
+
+/*
+
+
+    data: {
+        type: "training",
+        data: {
+            branch: "fms",
+            data: [
+                "item1",
+                "item2"
+            ]
+        }
+    },
+    data: {
+        type: "credentials",
+        data: {
+            branch: "fms",
+            data: [
+                "item1",
+                "item2"
+            ]
+        }
+    },
+
+*/
