24849888f7a1322aa0f3965c90fc9773e9351d64
David <onesimusrex@gmail.com>
2021-10-29 11:10:36 -0400

    updates

--- public/html/js/select2_vmg.js
+++ public/html/js/select2_vmg.js
@@ -1,131 +1,131 @@
-json = [
-    {
-        manager: "Manager1",
-        employees : 
-            [
-                "Liam",
-                "Olivia",
-                "Noah",
-                "Emma",
-                "Oliver",
-                "Ava",
-                "William",
-                "Sophia",
-                "Elijah"
-            ]
-    },
-    {
-        manager: "Manager2",
-        employees : 
-            [
-                "Isabella",
-                "James",
-                "Charlotte",
-                "Benjamin",
-                "Amelia",
-                "Lucas",
-                "Mia",
-                "Mason",
-                "Harper",
-                "Ethan",
-                "Evelyn"
-            ]
-    }
-]
+// json = [
+//     {
+//         manager: "Manager1",
+//         employees : 
+//             [
+//                 "Liam",
+//                 "Olivia",
+//                 "Noah",
+//                 "Emma",
+//                 "Oliver",
+//                 "Ava",
+//                 "William",
+//                 "Sophia",
+//                 "Elijah"
+//             ]
+//     },
+//     {
+//         manager: "Manager2",
+//         employees : 
+//             [
+//                 "Isabella",
+//                 "James",
+//                 "Charlotte",
+//                 "Benjamin",
+//                 "Amelia",
+//                 "Lucas",
+//                 "Mia",
+//                 "Mason",
+//                 "Harper",
+//                 "Ethan",
+//                 "Evelyn"
+//             ]
+//     }
+// ]
 
 $(document).ready(function() {
-    $('.js-example-basic-multiple').select2()  
+//     $('.js-example-basic-multiple').select2()  
 
-    $('#employee-select').select2({
-        placeholder: 'Select your employees',
-        closeOnSelect: false,
-        disabled: true
-    }) 
+//     $('#employee-select').select2({
+//         placeholder: 'Select your employees',
+//         closeOnSelect: false,
+//         disabled: true
+//     }) 
 
-    $('#manager-select').select2({
-        data: ListManagers(json),
-        placeholder: 'Select your name',
-    })  
+//     $('#manager-select').select2({
+//         data: ListManagers(json),
+//         placeholder: 'Select your name',
+//     })  
 
-    $('#manager-select').val(null).trigger('change');
+//     $('#manager-select').val(null).trigger('change');
 
-    $('#manager-select').on('select2:select', function (e) {
-        var data = e.params.data;
-        $('#employee-select').empty().trigger('change');
-        $('#employee-select').select2({
-            data: GetEmployees(json, data.text),
-            placeholder: 'Select your employees',
-            closeOnSelect: false,
-            disabled: false
-        }) 
-    });
+//     $('#manager-select').on('select2:select', function (e) {
+//         var data = e.params.data;
+//         $('#employee-select').empty().trigger('change');
+//         $('#employee-select').select2({
+//             data: GetEmployees(json, data.text),
+//             placeholder: 'Select your employees',
+//             closeOnSelect: false,
+//             disabled: false
+//         }) 
+//     });
 
-    $('#employee-select').on('select2:close', function (e) {
-        // var data = e.params.data;
-        var tempArr = []
-        $('#employee-select').find(':selected').each(function(i){
-            console.log($(this).text())
-            tempArr.push ($(this).text())
-        })
+//     $('#employee-select').on('select2:close', function (e) {
+//         // var data = e.params.data;
+//         var tempArr = []
+//         $('#employee-select').find(':selected').each(function(i){
+//             console.log($(this).text())
+//             tempArr.push ($(this).text())
+//         })
 
-        data = tempArr.map(function (item, i) {
-            item.id = i;
-            _item = {id:i, text:item}
-            return _item;
-        }); 
-        $('.tablerow').select2({
-            data: GetEmployees(json, data),
-            placeholder: 'Select your employees',
-            closeOnSelect: false,
-            disabled: false
-        }) 
-    });
-
-
+//         data = tempArr.map(function (item, i) {
+//             item.id = i;
+//             _item = {id:i, text:item}
+//             return _item;
+//         }); 
+//         $('.tablerow').select2({
+//             data: GetEmployees(json, data),
+//             placeholder: 'Select your employees',
+//             closeOnSelect: false,
+//             disabled: false
+//         }) 
+//     });
 
 
 
 
-// ListManagers(json);
-// GetEmployees(json, "Manager1")
 
-function AddSelect2(selector){
-    console.log(selector)
-    $(selector).select2({
-        data: GetEmployees(json, "Manager1")
-    });
-}
 
-function ListManagers(array, index){
-    data = array.map(function (item, i){
-        item.id = i;
-        _item = {id:i, text:item.manager}
-        return _item;
-    })
-    // console.log(data)
-    _result = {
-        'results':data
-    }
-    return data 
+// // ListManagers(json);
+// // GetEmployees(json, "Manager1")
+
+// function AddSelect2(selector){
+//     console.log(selector)
+//     $(selector).select2({
+//         data: GetEmployees(json, "Manager1")
+//     });
+// }
+
+// function ListManagers(array, index){
+//     data = array.map(function (item, i){
+//         item.id = i;
+//         _item = {id:i, text:item.manager}
+//         return _item;
+//     })
+//     // console.log(data)
+//     _result = {
+//         'results':data
+//     }
+//     return data 
     
-}
+// }
 
-function GetEmployees (array, manager){
-    array.forEach((element, index)=> {
-        if (manager == element.manager){
-            data = element.employees.map(function (item, i) {
-                item.id = i;
-                _item = {id:i, text:item}
-                return _item;
-            }); 
-            _result = {
-                'results':data
-            }
+// function GetEmployees (array, manager){
+//     array.forEach((element, index)=> {
+//         if (manager == element.manager){
+//             data = element.employees.map(function (item, i) {
+//                 item.id = i;
+//                 _item = {id:i, text:item}
+//                 return _item;
+//             }); 
+//             _result = {
+//                 'results':data
+//             }
              
-        }
-    });
-    return /*_result*/ data 
-}
+//         }
+//     });
+//     return /*_result*/ data 
+// }
 
 /*
 
@@ -256,13 +256,11 @@ function Manager(managerFirst, managerLast, managerEducation){
     function updateEmployeeDisplay(employee, remove){
         _this = this;
         if (!remove){
-            console.log('updater')
             var li = document.createElement('li');
             $(li).addClass('list-group-item').text(employee.firstName + " " + employee.lastName).append('<button id="'+ employee._id +'" type="button" class="btn btn-primary btn-sm float-end remove-item-bt"><i class="fas fa-user-times"></i>remove</button>')/*.attr('id', employee._id)*/
             $('#employee-list').append(li)
             $('#'+ employee._id ).on('click.test', function(event){
                 event.preventDefault();
-                console.log('riding');
                 $(this).parent('li').remove()
                 _this.removeEmployee(employee._id);
                 _this.updateEmployeeDisplay(employee, true)
